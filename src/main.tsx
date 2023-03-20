a661da5faa7420cc13573d969c7b35bae306f450
linjunjie <linjunjie.art@bytedance.com>
2023-03-21 01:29:49 +0800

    feat: 不知道写了点啥～

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import WebMonitor from "web-monitor"
-const m = new WebMonitor("", "http://www.error.example.com", "get", "xhr", 1);
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import WebMonitor from "web-monitor";
+const m = new WebMonitor(
+  "63e8a1189343016c82104082",
+  "https://hnwmjd.lafyun.com:443/addLogger",
+  "post",
+  "xhr",
+  1
+);
 m.start();
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
